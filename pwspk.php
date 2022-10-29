5eedaefd9c4262e5423c6755650b2822b7841ee5
oldman07 <backagain010@gmail.com>
2022-10-29 11:57:39 +0500

    tutorial 4

--- pwspk.php
+++ pwspk.php
@@ -7,13 +7,20 @@ register_activation_hook(__FILE__,'pwspk_register_activation_hook');
 register_deactivation_hook(__FILE__,'pwspk_register_deactivation_hook');
 register_uninstall_hook( __FILE__,'pwspk_register_uninstall_hook');
 
+//add_action is used to create our own hook
+//do_action is used to perform action on hooks created using add_action.
+//remove_action is used to remove  hooks created using add_action.
+//has_action is contitional statement use to check weather the hook is there or not
 
- function pwspk_register_activation_hook(){
-     add_option('updated_title','this title is hacked');  
-     //update_title is user define and it create an option in wp-admin/options.php
-     
-}
+add_action( 'init', 'pwspk_init' );  
 
-function pwspk_register_deactivation_hook(){
-    delete_option('updated_title');
-}
+function pwspk_init(){
+    add_shortcode( 'test', 'pwspk_my_shortcode' );
+    }
+    function pwspk_my_shortcode($atts)
+    {
+        $atts = shortcode_atts(array(
+                'message' => 'hello worlds',
+            ), $atts,'test');
+         return $atts['message'];
+    }
