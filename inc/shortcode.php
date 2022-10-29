5a2d3e6bab06f24d09c485362f1f3879a961b2c5
oldman07 <backagain010@gmail.com>
2022-10-29 12:39:23 +0500

    create new shortcode file

---
+++ inc/shortcode.php
@@ -0,0 +1,14 @@
+<?php
+add_action('init', 'pwspk_init');
+
+function pwspk_init()
+{
+    add_shortcode('test', 'pwspk_my_shortcode');
+}
+function pwspk_my_shortcode($atts)
+{
+    $atts = shortcode_atts(array(
+        'message' => 'hello worlds',
+    ), $atts, 'test');
+    return $atts['message'];
+}
