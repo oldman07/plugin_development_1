ed5a43a6a294465cd71eff973ef7760d5adc5ee7
oldman07 <backagain010@gmail.com>
2022-10-29 12:26:41 +0500

    applying filter and static resources

--- pwspk.php
+++ pwspk.php
@@ -3,9 +3,9 @@
 /**
  * Plugin Name: Pwsdk_plugin_test
  */
-register_activation_hook(__FILE__,'pwspk_register_activation_hook');
-register_deactivation_hook(__FILE__,'pwspk_register_deactivation_hook');
-register_uninstall_hook( __FILE__,'pwspk_register_uninstall_hook');
+// register_activation_hook(__FILE__,'pwspk_register_activation_hook');
+// register_deactivation_hook(__FILE__,'pwspk_register_deactivation_hook');
+// register_uninstall_hook( __FILE__,'pwspk_register_uninstall_hook');
 
 //add_action is used to create our own hook
 //do_action is used to perform action on hooks created using add_action.
@@ -24,3 +24,16 @@ function pwspk_init(){
             ), $atts,'test');
          return $atts['message'];
     }
+
+    add_filter( 'the_title' , 'pwspk_title' );
+function pwspk_title($title){
+    return "you title is hacked";
+}
+
+ add_action( 'wp_enqueue_scripts' ,'pwspk_adding_script');
+
+ function pwspk_adding_script(){
+      wp_enqueue_style( 'pwspk_plugin_dev', plugin_dir_url(__FILE__)."assets/css/style.css");
+
+      wp_enqueue_script( 'pwspk_plugin_dev', plugin_dir_url(__FILE__)."assets/js/custom.js");
+ }
