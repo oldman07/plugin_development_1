bd369cd7e06aaa6f5aa50b061b101b0d0c859a77
oldman07 <backagain010@gmail.com>
2022-10-30 15:09:09 +0500

    adding value to option.php

--- pwspk.php
+++ pwspk.php
@@ -3,8 +3,8 @@
 /**
  * Plugin Name: Pwsdk_plugin_test
  */
-// register_activation_hook(__FILE__,'pwspk_register_activation_hook');
-// register_deactivation_hook(__FILE__,'pwspk_register_deactivation_hook');
+register_activation_hook(__FILE__,'pwspk_register_activation_hook');
+register_deactivation_hook(__FILE__,'pwspk_register_deactivation_hook');
 // register_uninstall_hook( __FILE__,'pwspk_register_uninstall_hook');
 
 //add_action is used to create our own hook
@@ -12,15 +12,23 @@
 //remove_action is used to remove  hooks created using add_action.
 //has_action is contitional statement use to check weather the hook is there or not
 
+function pwspk_register_activation_hook(){
+    add_option( 'pwspk_option_1', '' );
+}
+
+function pwspk_register_deactivation_hook(){
+   delete_option( 'pwspk_option_1');
+}
+
 include plugin_dir_path(__FILE__)."/inc/shortcode.php";     //plugin_dir_path is use to add php files in the file
 
-add_filter('the_title', 'pwspk_title');
+add_filter('the_title', 'pwspk_title');                     //the_title is a pre define term use to make changes in post title.
 function pwspk_title($title)
 {
     return "you title is hacked";
 }
 
-add_action('wp_enqueue_scripts', 'pwspk_adding_script');
+add_action('wp_enqueue_scripts', 'pwspk_adding_script');        //wp_enqueue_scripts is user to add static resources like css and js.
 
 function pwspk_adding_script()
 {
@@ -29,7 +37,8 @@ function pwspk_adding_script()
     wp_enqueue_script('pwspk_plugin_dev', plugin_dir_url(__FILE__) . "assets/js/custom.js");
 }
 
-add_action( 'admin_menu','pwspk_options' );
+add_action( 'admin_menu','pwspk_options' );                                 // admin_menu  Fires before the administration menu loads in the admin
+add_action( 'admin_menu','pwspk_process_form_settings' );
 
 function pwspk_options(){
     add_menu_page( 'pwspk option','pwspk option', 'manage_options', 'pwspk-option', 'pwspk_option_func' );  //add_menu_page( string $page_title, string $menu_title, string $capability, string $menu_slug, callable $callback = '', string $icon_url = '', int|float $position = null ): string
@@ -37,8 +46,30 @@ function pwspk_options(){
     add_options_page( 'Theme option','Theme option', 'manage_options', 'pwspk_theme_Settings','pwspk_theme_func');
 }
 
-function pwspk_option_func(){
-    echo '<h1>pwspk_option</h1>';
+function pwspk_process_form_settings(){
+    register_setting( 'pwspk-option-group' ,'pwspk-option-name' );
+    if(isset($_POST['action']) && current_user_can( 'manage_options' ))
+    {
+      update_option( 'pwspk-option-group', sanitize_text_field( $_POST['pwspk_option_1'] ) );
+
+    };
+}
+
+function pwspk_option_func(){ ?>
+ 
+
+<div class="wrap">
+<h1>pwspk_option</h1>
+    <form action="options.php" method='post'>
+        <?php settings_fields( 'pwspk-option-group' )  ?>
+        <label for=""> Setting 1:
+        <input type="text" name='pwspk_option_1' value ="<?php esc_html( get_option('pwspk_option_1')); ?>" /></label>
+        <?php submit_button( 'Save changes' ) ?>
+    </form>
+</div>
+
+<?php
+    
 }
 function pwspk_settings_func(){
     echo '<h1>pwspk_settings</h1>';
