abf5898bb2cb6336f143fcd344f1488dbabfcbfb
oldman07 <backagain010@gmail.com>
2022-11-02 14:07:08 +0500

    adding meta boes

---
+++ inc/metaboxes.php
@@ -0,0 +1,31 @@
+<?php
+
+add_action('admin_init', 'pwspk_metaboxes');
+
+function pwspk_metaboxes()
+{
+    add_meta_box('_mycustommetabox', 'My Custom MetaBox', 'pwspk_custom_metabox', ['post','page']);               //add_meta_box( $id:string, $title:string, $callback:callable, $screen:string|array|WP_Screen|null, $context:string, $priority:string, $callback_args:array|null ) is use to add metaboxes
+}
+function pwspk_custom_metabox($post)
+{
+    $_mymetabox = get_post_meta($post->ID, '_mymetabox', true) ? get_post_meta($post->ID, '_mymetabox', true) : " ";            // $_mymetabox = get_post_meta($post->ID, '_mymetabox', true) $_mymetabox is a unique id and we are checking weather we have the item or not.
+    $_myselectbox = get_post_meta($post->ID, '_myselectbox', true) ? get_post_meta($post->ID, '_myselectbox', true) : " ";
+    ?>
+    <input type="text" id='' name='_mymetabox' class='' value="<?php echo $_mymetabox;?>">
+    <select name="_selectbox" id="">
+        <option value="1" <?php echo $_myselectbox == 1 ? "selected" : ''?>>One</option>
+        <option value="2" <?php echo $_myselectbox == 2 ? "selected" : ''?>>Two</option>
+        <option value="3" <?php echo $_myselectbox == 3 ? "selected" : ''?>>Three</option>
+    </select>
+<?php
+}
+
+add_action('save_post', 'pwspk_save_post');
+
+function pwspk_save_post($post_id)
+{
+    if (array_key_exists('_mymetabox', $_POST) && array_key_exists('_selectbox', $_POST)) {                 //array_key_exists('_mymetabox', $_POST) used to check weather that post id consist of that id or not
+        update_post_meta($post_id, '_mymetabox', $_POST['_mymetabox']);                                     //update_post_meta is used to update the meta of the given post
+        update_post_meta($post_id, '_myselectbox', $_POST['_selectbox']);
+    }
+}
--- pwspk.php
+++ pwspk.php
@@ -3,8 +3,8 @@
 /**
  * Plugin Name: Pwsdk_plugin_test
  */
-register_activation_hook(__FILE__,'pwspk_register_activation_hook');
-register_deactivation_hook(__FILE__,'pwspk_register_deactivation_hook');
+register_activation_hook(__FILE__, 'pwspk_register_activation_hook');
+register_deactivation_hook(__FILE__, 'pwspk_register_deactivation_hook');
 // register_uninstall_hook( __FILE__,'pwspk_register_uninstall_hook');
 
 //add_action is used to create our own hook
@@ -12,18 +12,22 @@ register_deactivation_hook(__FILE__,'pwspk_register_deactivation_hook');
 //remove_action is used to remove  hooks created using add_action.
 //has_action is contitional statement use to check weather the hook is there or not
 
- //php recommend to wrap in Div
+//php recommend to wrap in Div
 //settings_errors( ) use to show error on front end.
 
-function pwspk_register_activation_hook(){
-    add_option( 'pwspk_option_1', '' );
+function pwspk_register_activation_hook()
+{
+    add_option('pwspk_option_1', '');
 }
 
-function pwspk_register_deactivation_hook(){
-   delete_option( 'pwspk_option_1');
+function pwspk_register_deactivation_hook()
+{
+    delete_option('pwspk_option_1');
 }
 
 include plugin_dir_path(__FILE__)."/inc/shortcode.php";     //plugin_dir_path is use to add php files in the file
+include plugin_dir_path(__FILE__)."/inc/metaboxes.php";
+
 
 add_filter('the_title', 'pwspk_title');                     //the_title is a pre define term use to make changes in post title.
 function pwspk_title($title)
@@ -35,51 +39,54 @@ add_action('wp_enqueue_scripts', 'pwspk_adding_script');        //wp_enqueue_scr
 
 function pwspk_adding_script()
 {
-    wp_enqueue_style('pwspk_plugin_dev', plugin_dir_url(__FILE__) . "assets/css/style.css"); //plugin_dir_url is use to add static resources 
+    wp_enqueue_style('pwspk_plugin_dev', plugin_dir_url(__FILE__) . "assets/css/style.css"); //plugin_dir_url is use to add static resources
 
     wp_enqueue_script('pwspk_plugin_dev', plugin_dir_url(__FILE__) . "assets/js/custom.js");
 }
 
-add_action( 'admin_menu','pwspk_options' );                                 // admin_menu  Fires before the administration menu loads in the admin
-add_action( 'admin_menu','pwspk_process_form_settings' );                   //process form settings allow us to redirect our form
+add_action('admin_menu', 'pwspk_options');                                 // admin_menu  Fires before the administration menu loads in the admin
+add_action('admin_menu', 'pwspk_process_form_settings');                   //process form settings allow us to redirect our form
 
-function pwspk_options(){
-    add_menu_page( 'pwspk option','pwspk option', 'manage_options', 'pwspk-option', 'pwspk_option_func' );  //add_menu_page( string $page_title, string $menu_title, string $capability, string $menu_slug, callable $callback = '', string $icon_url = '', int|float $position = null ): string
-    add_submenu_page('pwspk-option', 'pwspk settings', 'pwspk settings', 'manage_options', 'pwspk settings','pwspk_settings_func' );   //add_submenu_page( string $parent_slug, string $page_title, string $menu_title, string $capability, string $menu_slug, callable $callback = '', int|float $position = null ):
-    add_options_page( 'Theme option','Theme option', 'manage_options', 'pwspk_theme_Settings','pwspk_theme_func');
+function pwspk_options()
+{
+    add_menu_page('pwspk option', 'pwspk option', 'manage_options', 'pwspk-option', 'pwspk_option_func');  //add_menu_page( string $page_title, string $menu_title, string $capability, string $menu_slug, callable $callback = '', string $icon_url = '', int|float $position = null ): string
+    add_submenu_page('pwspk-option', 'pwspk settings', 'pwspk settings', 'manage_options', 'pwspk settings', 'pwspk_settings_func');   //add_submenu_page( string $parent_slug, string $page_title, string $menu_title, string $capability, string $menu_slug, callable $callback = '', int|float $position = null ):
+    add_options_page('Theme option', 'Theme option', 'manage_options', 'pwspk_theme_Settings', 'pwspk_theme_func');
 }
 
-function pwspk_process_form_settings(){
-    register_setting( 'pwspk-option-group' ,'pwspk-option-name' );
-    if(isset($_POST['action']) && current_user_can( 'manage_options' ))
-    {
-      update_option( 'pwspk_option_1', sanitize_text_field( $_POST['pwspk_option_1'] ) );
-
-    };                                                                                                  
+function pwspk_process_form_settings()
+{
+    register_setting('pwspk-option-group', 'pwspk-option-name');
+    if (isset($_POST['action']) && current_user_can('manage_options')) {
+        update_option('pwspk_option_1', sanitize_text_field($_POST['pwspk_option_1']));
+    };
 }
 
-function pwspk_option_func(){ ?>
+function pwspk_option_func()
+{ ?>
  
 
 <div class="wrap">                                                 
 <h1>pwspk_option</h1>
-<?php settings_errors( ) ?>
+<?php settings_errors() ?>
 
     <form action="options.php" method='post'>
-        <?php settings_fields( 'pwspk-option-group' )  ?>
+        <?php settings_fields('pwspk-option-group')  ?>
         <label for=""> Setting 1:
-        <input type="text" name='pwspk_option_1' value ="<?php esc_html( get_option('pwspk_option_1')); ?>" /></label>
-        <?php submit_button( 'Save changes' ) ?>
+        <input type="text" name='pwspk_option_1' value ="<?php esc_html(get_option('pwspk_option_1')); ?>" /></label>
+        <?php submit_button('Save changes') ?>
     </form>
 </div>
 
 <?php
-    
+
 }
-function pwspk_settings_func(){
+function pwspk_settings_func()
+{
     echo '<h1>pwspk_settings</h1>';
 }
 
-function pwspk_theme_func(){
+function pwspk_theme_func()
+{
     echo '<h1>pwspk_Theme_settings</h1>';
 }
